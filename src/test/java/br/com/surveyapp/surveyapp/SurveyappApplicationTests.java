b8198e35976e856cb409da4ac7a59412b651c735
Lucas PC <lucasbendasantos@hotmail.com>
2022-03-21 22:20:15 -0300

    other files

---
+++ src/test/java/br/com/surveyapp/surveyapp/SurveyappApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.surveyapp.surveyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
